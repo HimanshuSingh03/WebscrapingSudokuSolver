3d8d93163bb9d679315fae66e38a0e94f9edb8c3
HimanshuSingh03 <72284067+HimanshuSingh03@users.noreply.github.com>
2022-05-24 17:16:57 -0400

    completed sudoku solver

--- main.py
+++ main.py
@@ -10,25 +10,44 @@ grid = [[5,3,0,0,7,0,0,0,0],
         [0,0,0,4,1,9,0,0,5],
         [0,0,0,0,8,0,0,7,9]]
 
-print(np.matrix(grid))
 
 def fitHere(y,x,n):
+
+        #print("checking ",n)
         for i in range(9):
                 if grid[y][i] == n: #check rows
                         return False
                 if grid[i][x] == n: #check columns
                         return False
         
-        xsquare = x//3 #can only be 0,1,2
         ysquare = y//3
+        xsquare = x//3 #can only be 0,1,2
+        
+        #print(ysquare,xsquare)
 
         for i in range(3):
                 for j in range(3):
-                        if grid[(xsquare*3 + 1)+i][(xsquare*3 + 1)+i] == n:
+                        if grid[(ysquare*3)+i][(xsquare*3)+j] == n:
                                 return False
 
         return True
 
+def solve():
+        global grid
+        for y in range(9):
+                for x in range(9):
+                        if grid[y][x] == 0:
+                                for n in range(1,10):
+                                        if fitHere(y,x,n):
+                                                grid[y][x] = n
+                                                solve()
+                                                grid[y][x] = 0
+                                return
+        print(np.matrix(grid))
 
-print(fitHere(0,2,6))
+def main():
+        print("Current Sudoku: \n", np.matrix(grid))
+        print("\n Solved Sudoku:")
+        solve()
 
+main()
