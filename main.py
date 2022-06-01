6182b32e8e6f60be81170628cf431b6b8d5d686c
HimanshuSingh03 <72284067+HimanshuSingh03@users.noreply.github.com>
2022-06-01 15:51:17 -0400

    completed arrange, error in main

--- main.py
+++ main.py
@@ -4,71 +4,66 @@ import numpy as np
 
 
 tempgrid = []
+counter = 0
+grid = []
 
 
-req = requests.get("https://sudokugenerator.com/sudoku/daily/")
-soup = BeautifulSoup(req.content, "html.parser")
+def scrape():
+        print("scraping...")
+        
+        req = requests.get("https://sudokugenerator.com/sudoku/daily/")
+        soup = BeautifulSoup(req.content, "html.parser")
 
-rows = soup.find_all('input', type="text")
+        rows = soup.find_all('input', type="text")
 
-for row in rows:
-        value = row.get('value')
-        tempgrid.append(value)
+        for row in rows:
+                value = row.get('value')
+                tempgrid.append(value)
+        
+        return tempgrid
 
 
 
 
 def arrangegrid(tempgrid):
-        
-        counter = 0
-        grid = []
-        for i in range(9):
+        global counter
+        global grid
+
+        print("arranging...")
+        for i in range(3):
                 row_array = []
                 
-                
-                if i == 3:
-                        counter = 1
-                elif i == 6:
-                        counter = 2
 
                 for j in range(3):
 
                         index = ((9*j) + 3*i) + counter*27
                         row_array.append(tempgrid[index])
-                        print(index)
-
+                        
                         index = ((9*j +1) + 3*i) + counter*27
                         row_array.append(tempgrid[index])
-                        print(index)
-
+                        
                         index = ((9*j +2) + 3*i) + counter*27
                         row_array.append(tempgrid[index])
-                        print(index)
-
+                        
                 grid.append(row_array)
-                #print(row_array)
-        # print(grid)
+        
+        counter +=1
+
+        if counter != 3:
+                arrangegrid(tempgrid)
+        else:
+                return grid
 
 
-arrangegrid(tempgrid)
 
 
 
 
-# grid = [[5,3,0,0,7,0,0,0,0],
-#         [6,0,0,1,9,5,0,0,0],
-#         [0,9,8,0,0,0,0,6,0],
-#         [8,0,0,0,6,0,0,0,3],
-#         [4,0,0,8,0,3,0,0,1],
-#         [7,0,0,0,2,0,0,0,6],
-#         [0,6,0,0,0,0,2,8,0],
-#         [0,0,0,4,1,9,0,0,5],
-#         [0,0,0,0,8,0,0,7,9]]
 
 
 def fitHere(y,x,n):
-
-        #print("checking ",n)
+        global grid
+        
         for i in range(9):
                 if grid[y][i] == n: #check rows
                         return False
@@ -89,6 +84,8 @@ def fitHere(y,x,n):
 
 def solve():
         global grid
+
+        print("solving...")
         for y in range(9):
                 for x in range(9):
                         if grid[y][x] == 0:
@@ -101,7 +98,13 @@ def solve():
         print(np.matrix(grid))
 
 def main():
-        print("Current Sudoku: \n", np.matrix(grid))
-        print("\n Solved Sudoku:")
-        solve()
 
+        tempgrid = scrape()
+        print(tempgrid)
+        sudoku = arrangegrid(tempgrid)
+        print(sudoku)
+        print("Current Sudoku: \n", np.matrix(sudoku))
+        # print("\n Solved Sudoku:")
+        # solve()
+
+main()
